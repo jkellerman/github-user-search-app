7ffa107734642dab3d1b5a908e3f44f47af1d46a
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-03-23 20:00:16 +0000

    add date formatter in script

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,22 +1,39 @@
-const url = "https://api.github.com/users/bradtraversy";
+const url = "https://api.github.com/users/octocat";
 
 const getUser = async () => {
   const response = await fetch(url);
   const jsonResponse = await response.json();
   if (response.ok) {
     console.log(jsonResponse);
+
+    // get name
     let name = document.querySelector(".name");
     if (jsonResponse.name === "" || jsonResponse.name === null) {
       name.innerText = jsonResponse.login.replace(/\@/g, "");
     } else {
       name.innerText = jsonResponse.name;
     }
+
+    // get username
     let username = (document.querySelector(
       ".username"
     ).innerText = `@${jsonResponse.login}`);
-    let dateJoined = (document.querySelector(".joined").innerText =
-      jsonResponse.created_at);
+
+    // get date joined
+    let dateJoined = document.querySelector(".joined");
+    let date = new Date(jsonResponse.created_at);
+    let dtFormat = new Intl.DateTimeFormat("en-GB", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+    });
+    let newDate = dtFormat.format(date);
+    dateJoined.innerText = `Joined ${newDate}`;
+
+    // get image icon
     let icon = (document.querySelector("#icon").src = jsonResponse.avatar_url);
+
+    // get bio
     let bio = document.querySelector(".bio");
     if (jsonResponse.bio === "" || jsonResponse.bio === null) {
       bio.innerText = "This profile has no bio";
@@ -24,12 +41,16 @@ const getUser = async () => {
     } else {
       bio.innerText = jsonResponse.bio;
     }
+
+    // get stats
     let repos = (document.querySelector(".repos").innerText =
       jsonResponse.public_repos);
     let followers = (document.querySelector(".followers").innerText =
       jsonResponse.followers);
     let following = (document.querySelector(".following").innerText =
       jsonResponse.following);
+
+    // get info
     let location = document.querySelector(".location");
     if (jsonResponse.location === "" || jsonResponse.location === null) {
       renderNotAvailable(location);
@@ -54,7 +75,7 @@ const getUser = async () => {
       twitter.href = `https://twitter.com/${jsonResponse.twitter_username}`;
     }
     let company = document.querySelector(".company");
-    if (!jsonResponse.company.includes("@")) {
+    if (jsonResponse.company === null || !jsonResponse.company.includes("@")) {
       renderNotAvailable(company);
     } else {
       company.innerText = jsonResponse.company;
